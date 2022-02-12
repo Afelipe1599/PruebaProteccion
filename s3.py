9b422d7d49ec659b3d698d0992454f178fb0525a
afelipe1599 <andres.montes@correounivalle.edu.co>
2022-02-12 12:32:53 -0500

    code fix v1

--- s3.py
+++ s3.py
@@ -1,26 +1,25 @@
 """Boto3 Module"""
 import boto3
 
-#Metodo que sube un archivo a S3
+
 def upload_file(name, bucket,object_name):
+    "Function upload files"
     s3_client = boto3.client('s3')
     response = s3_client.upload_file(name,bucket,object_name)
     return response
 
-#Metodo que descarga un archivo de S3
 def download_file(name,bucket):
-    s3 = boto3.resource('s3')
+    "Function download file"
+    s3_client = boto3.resource('s3')
     output = f"downloads/{name}"
-    s3.Bucket(bucket).download_file(name, output)
+    s3_client.Bucket(bucket).download_file(name, output)
     return output
 
-#Metodo para listar archivos de S3
 def list_file(bucket):
+    "Function list files"
     s3_client = boto3.client('s3')
     contents = []
-    try:
+    if 'Contents' in s3_client.list_objects(Bucket=bucket):
         for item in s3_client.list_objects(Bucket=bucket)['Contents']:
             contents.append(item)
-        return contents
-    except:
-        return contents
+    return contents
--- server.py
+++ server.py
@@ -1,47 +1,46 @@
 """Module os"""
 import os
 
-from flask import (Flask,
-                  request, 
-                  render_template,
-                   redirect,
-                   send_file)
+from flask import (Flask,request,render_template,redirect, send_file,send_from_directory)
 
-from s3 import (upload_file,
-                download_file,
-                list_file)
+from flask_wtf.csrf import CSRFProtect
 
-from resize import resize                
+from s3 import (upload_file,download_file,list_file)
+
+from resize import resize
 
 app = Flask(__name__)
+#csrf = CSRFProtect()
+#csrf.init_app(app) # Compliant
 
 UPLOAD_FOLDER = "uploads"
 BUCKET = "ovasbucker"
 
 @app.route("/")
 def home():
+    "Function home"
     contents = list_file("ovasbucker")
     return render_template('index.html', contents=contents)
 
 
 @app.route('/upload', methods=['POST'])
 def upload():
+    "Function upload"
     if request.method == "POST":
-        f = request.files.getlist('file')     
-        for file in  f :
+        file_array = request.files.getlist('file')
+        for file in  file_array :
             file.save(os.path.join(UPLOAD_FOLDER, file.filename))
             resize(f"uploads/{file.filename}")
             upload_file(f"uploads/{file.filename}", BUCKET,file.filename)
-        return redirect("/")
-        
+    return redirect("/")
 
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
+    "Function download"
     if request.method == 'GET':
         output = download_file(filename, BUCKET)
+    return send_file(output, as_attachment=True)
+    
 
-        return send_file(output, as_attachment=True)
-       
 if __name__ == "__main__":
     app.run(debug=True,host="localhost",port="5000")
-
